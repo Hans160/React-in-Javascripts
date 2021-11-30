2a8ad3173089b9ff2c6e19d8f6e9e617b8b8daed
hans160 <cyclone160@naver.com>
2021-11-30 23:12:30 +0900

    Mypage.js 함수 추가

--- twi/src/Pages/Mypage.js
+++ twi/src/Pages/Mypage.js
@@ -2,3 +2,7 @@ import React from 'react';
 import { dummyTweets } from '../static/dummyData';
 import './MyPage.css';
 import Footer from '../Footer';
+
+const MyPage = () => {};
+
+export default MyPage;
