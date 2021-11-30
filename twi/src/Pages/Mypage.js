9a54436717b62a24584b658c8cb7485073bfbd15
hans160 <cyclone160@naver.com>
2021-11-30 23:15:54 +0900

    Mypage.js 필터함수

--- twi/src/Pages/Mypage.js
+++ twi/src/Pages/Mypage.js
@@ -3,6 +3,11 @@ import { dummyTweets } from '../static/dummyData';
 import './MyPage.css';
 import Footer from '../Footer';
 
-const MyPage = () => {};
+const MyPage = () => {
+  const filteredTweets = dummyTweets.filter(
+    (data) => data.username === 'songfront'
+  );
+  console.log('더미 파라미터');
+};
 
 export default MyPage;
