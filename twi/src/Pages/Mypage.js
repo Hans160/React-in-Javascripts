a415bca59f2ecbc5e0953d60655f74fe509f31ef
hans160 <cyclone160@naver.com>
2021-11-30 23:17:38 +0900

    Mypage.js Footer.js 추가 및 div 구조추가

--- twi/src/Pages/Mypage.js
+++ twi/src/Pages/Mypage.js
@@ -8,6 +8,25 @@ const MyPage = () => {
     (data) => data.username === 'songfront'
   );
   console.log('더미 파라미터');
+
+  return (
+    <section className="myInfo">
+      <div className="myInfo__container">
+        <div className="myInfo__wrapper">
+          <div className="myInfo__profile">
+            <img src={filteredTweets[0].picture} />
+          </div>
+          <div className="myInfo__detail">
+            <p className="myInfo__detailName">
+              {filteredTweets[0].username} Profile
+            </p>
+            <p>28 팔로워 100 팔로잉</p>
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </section>
+  );
 };
 
 export default MyPage;
