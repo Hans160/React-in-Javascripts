ba732c27dc14653b54effc8a2f7d562c02175064
hans160 <cyclone160@naver.com>
2021-12-26 08:17:24 +0900

    stringifyJSON.js 추가

---
+++ stringify/src/stringifyJSON.js
@@ -0,0 +1,63 @@
+/**
+ * 1. Browser에 존재하는 JSON.stringfy 함수를 직접 구현해 봅니다.
+ * JSON.stringfy 함수는 input 값을 JSON 형식으로 변환합니다.
+ * 단, undefined와 function은 JSON으로 생략되거나 null로 변환됩니다.
+ *
+ * 2. stringfyJSON은 아래와 같이 작동합니다.
+ * - Boolean이 input으로 주어졌을 경우
+ * stringifyJSON(true);                // 'true'
+ * - String이 input으로 주어졌을 경우
+ * stringifyJSON('foo');               // '"foo"'
+ * - Array가 input으로 주어졌을 경우
+ * stringifyJSON([1, 'false', false]); // '[1,"false",false]'
+ * - Object가 input으로 주어졌을 경우
+ * stringifyJSON({ x: 5 });            // '{"x":5}'
+ * - undefined, function이 주어졌을 경우
+ * stringifyJSON(undefined)            // undefined
+ * stringifyJSON(function(){})         // undefined
+ * stringifyJSON({ x: undefined, y: function(){} })   // '{}'
+ *
+ * 3. spec/stringifyJSONSpec.js의 stringifiableObjects 배열을 참고해서 테스트에서 어떤 input 값들이
+ * 주어지고, 어떻게 stringify해 주어야 할지 생각해 보세요.
+ *
+ * 4. 그냥 테스트 통과를 하고 싶으시다면, 다음과 같이 구현하면 될 거예요.
+ *  const stringifyJSON = JSON.stringify;
+ */
+
+function stringifyJSON(obj) {
+  if (obj === null) {
+    return 'null';
+  }
+  if (typeof obj === 'number' || typeof obj === 'boolean') {
+    return String(obj);
+  }
+  if (typeof obj === 'string') {
+    return `"${obj}"`;
+  }
+  //obj가 배열인 경우
+  if (Array.isArray(obj)) {
+    const result = [];
+    obj.forEach(function (element) {
+      result.push(stringifyJSON(element));
+    });
+    return `[${result}]`;
+  }
+  //obj가 객체인 경우 (객체에는 배열, 객체 모두 포함되지만 배열인 경우보다 아래에 작성했으므로 배열은 제외된다.)
+  if (typeof obj === 'object') {
+    let result = '';
+    for (let key in obj) {
+      if (obj[key] === undefined || typeof obj[key] === 'function') {
+        result = String(result);
+      } else {
+        result = result + `${stringifyJSON(key)}:${stringifyJSON(obj[key])},`;
+      }
+    }
+    result = result.substr(0, result.length - 1);
+    return `{${result}}`;
+  }
+}
+
+// 다음 코드는 결과 제출을 위한 코드입니다. 신경 쓰지 않아도 좋습니다.
+if (typeof window === 'undefined') {
+  module.exports = stringifyJSON;
+}
